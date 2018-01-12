414662341da4828beb26536861ba3d24faa59fd4
damienyf <damienyf@gmail.com>
2018-01-12 02:14:16 -0600

    multithread

--- lxf-python-advanced/process-thread.py
+++ lxf-python-advanced/process-thread.py
@@ -83,3 +83,135 @@ print('Exit code:', p.returncode)
 # 由于Windows没有fork调用，因此，multiprocessing需要“模拟”出fork的效果，
 # 父进程所有Python对象都必须通过pickle序列化再传到子进程去，
 # 如果multiprocessing在Windows下调用失败了，要先考虑是不是pickle失败了。
+
+
+
+# 多任务可以由多进程完成，也可以由一个进程内的多线程完成。
+# 我们前面提到了进程是由若干线程组成的，一个进程至少有一个线程。
+
+# 于线程是操作系统直接支持的执行单元，因此，高级语言通常都内置多线程的支持，
+# Python也不例外，并且，Python的线程是真正的Posix Thread，而不是模拟出来的线程。
+# Python的标准库提供了两个模块：_thread和threading，_thread是低级模块，
+# threading是高级模块，对_thread进行了封装。
+# 绝大多数情况下，我们只需要使用threading这个高级模块。
+
+
+# 启动一个线程就是把一个函数传入并创建Thread实例，然后调用start()开始执行：
+import time, threading
+
+def loop():
+    # current_thread()函数，它永远返回当前线程的实例。
+    print('thread %s is running...' % threading.current_thread().name)
+    n = 0
+    while n < 5:
+        n = n + 1
+
+        print('thread %s >>> %s' % (threading.current_thread().name, n))
+
+        time.sleep(1)
+    print('thread %s ended.' % threading.current_thread().name)
+
+# 主线程实例的名字叫MainThread
+print('thread %s is running...' % threading.current_thread().name)
+# 子线程的名字在创建时指定，名字仅仅在打印时用来显示，完全没有其他意义
+t = threading.Thread(target=loop, name='LoopThread')
+# start()开始执行
+t.start()
+# join()方法可以等待子进程结束后再继续往下运行，通常用于进程间的同步。
+t.join()
+print('thread %s ended.' % threading.current_thread().name)
+
+
+# Lock
+# 来看看多个线程同时操作一个变量怎么把内容给改乱了
+import time, threading
+
+# 假定这是你的银行存款:
+balance = 0
+
+def change_it(n):
+    # 先存后取，结果应该为0:
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+def run_thread(n):
+    for i in range(10000000):
+        change_it(n)
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
+
+
+# t1和t2是交替运行的，如果操作系统以下面的顺序执行t1、t2：
+# 初始值 balance = 0
+
+t1: x1 = balance + 5  # x1 = 0 + 5 = 5
+
+t2: x2 = balance + 8  # x2 = 0 + 8 = 8
+t2: balance = x2      # balance = 8
+
+t1: balance = x1      # balance = 5
+t1: x1 = balance - 5  # x1 = 5 - 5 = 0
+t1: balance = x1      # balance = 0
+
+t2: x2 = balance - 8  # x2 = 0 - 8 = -8
+t2: balance = x2   # balance = -8
+
+# 结果 balance = -8
+
+
+
+# 要确保balance计算正确，就要给change_it()上一把锁
+# 锁只有一个，无论多少线程，同一时刻最多只有一个线程持有该锁
+# 通过threading.Lock()来实现
+
+balance = 0
+lock = threading.Lock()
+
+def run_thread(n):
+    for i in range(100000):
+        # 先要获取锁:
+        lock.acquire()
+        try:
+            # 放心地改吧:
+            change_it(n)
+        finally:
+            # 改完了一定要释放锁:
+            lock.release()
+# 获得锁的线程用完后一定要释放锁，否则那些苦苦等待锁的线程将成为死线程。
+# 所以我们用try...finally来确保锁一定会被释放。
+
+# 包含锁的某段代码实际上只能以单线程模式执行
+# 其次，由于可以存在多个锁，不同的线程持有不同的锁，
+# 并试图获取对方持有的锁时，可能会造成死锁，导致多个线程全部挂起
+
+
+
+# 多核CPU
+# 试试用Python写个死循环：
+
+import threading, multiprocessing
+
+def loop():
+    x = 0
+    while True:
+        x = x ^ 1
+
+for i in range(multiprocessing.cpu_count()):
+    t = threading.Thread(target=loop)
+    t.start()
+# 启动与CPU核心数量相同的N个线程，在4核CPU上可以监控到CPU占用率仅有102%，也就是仅使用了一核。
+# 但是用C、C++或Java来改写相同的死循环，直接可以把全部核心跑满，4核就跑到400%，8核就跑到800%，为什么Python不行呢？
+
+
+# 因为Python的线程虽然是真正的线程，但解释器执行代码时，有一个GIL锁：Global Interpreter Lock，
+# 任何Python线程执行前，必须先获得GIL锁，然后，每执行100条字节码，解释器就自动释放GIL锁，让别的线程有机会执行。
+# 这个GIL全局锁实际上把所有线程的执行代码都给上了锁，所以，多线程在Python中只能交替执行，即使100个线程跑在100核CPU上，也只能用到1个核。
+
+# Python虽然不能利用多线程实现多核任务，但可以通过多进程实现多核任务。
---
+++ lxf-python-advanced/threadlocal.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# 在多线程环境下，每个线程都有自己的数据。
+
+但是局部变量也有问题，就是在函数调用的时候，传递起来很麻烦：
+def process_student(name):
+    std = Student(name)
+    # std是局部变量，但是每个函数都要用它，因此必须传进去：
+    do_task_1(std)
+    do_task_2(std)
+
+def do_task_1(std):
+    do_subtask_1(std)
+    do_subtask_2(std)
+
+def do_task_2(std):
+    do_subtask_2(std)
+    do_subtask_2(std)
+
+# 每个函数一层一层调用都这么传参数那还得了？用全局变量？也不行，因为每个线程处理不同的Student对象，不能共享。
+# 如果用一个全局dict存放所有的Student对象，然后以thread自身作为key获得线程对应的Student对象如何？
+
+global_dict = {}
+
+def std_thread(name):
+    std = Student(name)
+    # 把std放到全局变量global_dict中：
+    global_dict[threading.current_thread()] = std
+    do_task_1()
+    do_task_2()
+
+def do_task_1():
+    # 不传入std，而是根据当前线程查找：
+    std = global_dict[threading.current_thread()]
+    ...
+
+def do_task_2():
+    # 任何函数都可以查找出当前线程的std变量：
+    std = global_dict[threading.current_thread()]
+    ...
+
+# 种方式理论上是可行的，它最大的优点是消除了std对象在每层函数中的传递问题，但是，每个函数获取std的代码有点丑。
+
+# ThreadLocal应运而生，不用查找dict，ThreadLocal帮你自动做这件事：
+
+import threading
+
+# 创建全局ThreadLocal对象:
+local_school = threading.local()
+
+def process_student():
+    # 获取当前线程关联的student:
+    std = local_school.student
+    print('Hello, %s (in %s)' % (std, threading.current_thread().name))
+
+def process_thread(name):
+    # 绑定ThreadLocal的student:
+    local_school.student = name
+    process_student()
+
+t1 = threading.Thread(target=process_thread, args=('Alice',), name='Thread-A')
+t2 = threading.Thread(target=process_thread, args=('Bob',), name='Thread-B')
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+
+# ThreadLocal最常用的地方就是为每个线程绑定一个数据库连接，HTTP请求，用户身份信息等，
+# 这样一个线程的所有调用到的处理函数都可以非常方便地访问这些资源。
+
+# 一个ThreadLocal变量虽然是全局变量，但每个线程都只能读写自己线程的独立副本，互不干扰。
+# ThreadLocal解决了参数在一个线程中各个函数之间互相传递的问题。
+
+# 异步IO
+# 考虑到CPU和IO之间巨大的速度差异，一个任务在执行的过程中大部分时间都在等待IO操作，
+# 单进程单线程模型会导致别的任务无法并行执行，因此，我们才需要多进程模型或者多线程模型来支持多任务并发执行。
+
+# 现代操作系统对IO操作已经做了巨大的改进，最大的特点就是支持异步IO。
+# 如果充分利用操作系统提供的异步IO支持，就可以用单进程单线程模型来执行多任务，
+# 这种全新的模型称为事件驱动模型，Nginx就是支持异步IO的Web服务器，它在单核CPU上采用单进程模型就可以高效地支持多任务。
+# 在多核CPU上，可以运行多个进程（数量与CPU核心数相同），充分利用多核CPU。
+# 由于系统总的进程数量十分有限，因此操作系统调度非常高效。用异步IO编程模型来实现多任务是一个主要的趋势。
+
+# 对应到Python语言，单线程的异步编程模型称为协程，有了协程的支持，就可以基于事件驱动编写高效的多任务程序。我们会在后面讨论如何编写协程。
